cd3a18147bb266d854ee1b7f923f69e206db58e2
Zach Blume <zachblume@gmail.com>
2024-06-01 22:43:13 -0400

    working

--- src/pages/api/readFromDb.js
+++ src/pages/api/readFromDb.js
@@ -9,7 +9,7 @@ const pool = new pg.Pool({
 
 export default async (req, res) => {
   // We want to read all the rows on the table first_table
-  const { rows } = await pool.query('SELECT * FROM first_table');
+  const { rows } = await pool.query('SELECT * FROM first_table');// WHERE org_id = $1');
   // send it back as json
   res.status(200).json(rows);
 }
